ce243c1582ebd01768503d833140f9d33e616feb
BlueMAN <nimazadeh963@outlook.com>
2026-08-16 14:25:22 +0000

    complete  updateUserPassword method in user.controller.js file

--- 08-E-Commerce API/controllers/user.controller.js
+++ 08-E-Commerce API/controllers/user.controller.js
@@ -29,7 +29,25 @@ const updateUser = async (req, res) => {
 };
 
 const updateUserPassword = async (req, res) => {
-  res.send("updated user password");
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError("please  provide both values");
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = user.comparePassword(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid credential");
+  }
+
+  user.password = newPassword;
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({});
 };
 
 module.exports = {
