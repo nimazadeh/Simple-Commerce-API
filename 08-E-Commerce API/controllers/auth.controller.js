ebd656d1e09b7ee7a14c9b52b3617c56423c5c44
BlueMAN <nimazadeh963@outlook.com>
2026-08-17 06:54:13 +0000

    update status code in login method

--- 08-E-Commerce API/controllers/auth.controller.js
+++ 08-E-Commerce API/controllers/auth.controller.js
@@ -47,7 +47,7 @@ const login = async (req, res) => {
 
   attachCookiesToResponse({ res, user: tokenUser });
 
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
